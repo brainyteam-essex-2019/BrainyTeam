73c5fdfb352fbeb454437ea698f0de443681459c
nwiske <nwiske@essex.ac.uk>
2019-02-28 10:24:00 +0000

    Created main file to execute system

---
+++ src/Main.py
@@ -0,0 +1,17 @@
+import featureExtraction
+import PreProcessing
+import ModelSaver
+import GenSVM
+import LDAClassifier
+
+"""
+Inputs needed
+1: Retrain models or not 
+2: use PCA or ICA
+3: What Window size to use? 
+
+
+"""
+
+
+
